bbcac076193276f113707c6beb9f899962aaa1ea
hunghly <hunghly@gmail.com>
2020-06-03 20:29:01 -0500

    added quadratic formula calculation

--- main.cpp
+++ main.cpp
@@ -52,6 +52,32 @@ int main()
 
     std::cout << "My name is Arya. Ruff ruff, I am " << human_years << " years old in human years.\n";
     */
+
+    /*
+    * Quadratic calcuation
+    double a;
+    double b;
+    double c;
+
+    std::cout << "Enter a: \n";
+    std::cin >> a;
+
+    std::cout << "Enter b: \n";
+    std::cin >> b;
+
+    std::cout << "Enter c: \n";
+    std::cin >> c;
+
+    double root1;
+    double root2;
+
+    // std::cout << pow(2, 2);
+    root1 = (-b + (sqrt(pow(b, 2) - (4 * a * c)))) / (2 * a);
+    root2 = (-b - (sqrt(pow(b, 2) - (4 * a * c)))) / (2 * a);
+
+    std::cout << "Root 1 is " << root1 << "\n";
+    std::cout << "Root 2 is " << root2 << "\n";
+    */
 }
 
 int dogYears()
