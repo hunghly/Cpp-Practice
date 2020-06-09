df23c1c2fb382c2ed468fbd56a1700cf62f3b1f3
hunghly <hunghly@gmail.com>
2020-06-08 21:58:30 -0500

    added fizzbuzz

--- main.cpp
+++ main.cpp
@@ -438,6 +438,21 @@ int main()
       std::cout << "You lose!\n";
     }
   */
+
+  /*
+    // fizzbuzz
+  for (int i = 1; i <= 100; i++) {
+    if (i % 15 == 0) {
+      std::cout << "FizzBuzz\n";
+    } else if (i % 3 == 0) {
+      std::cout << "Fizz\n";
+    } else if (i % 5 == 0) {
+      std::cout << "Buzz\n";
+    } else{
+      std::cout << i << "\n";
+    }
+  }
+  */
 }
 
 int dogYears()
